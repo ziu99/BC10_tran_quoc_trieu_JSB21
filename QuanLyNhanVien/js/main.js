006248ee1c464e7afd32ccba91472e30d82bae62
Trieu <trieu186168@gmail.com>
2024-01-12 12:34:00 +0700

    edit 5

--- QuanLyNhanVien/js/main.js
+++ QuanLyNhanVien/js/main.js
@@ -91,3 +91,71 @@ document.getElementById("btnCapNhat").onclick = function () {
 };
 
 // ...
+
+// ! TÌM NV XUẤT SẮC
+function timNhanVienXuatSac() {
+  var nhanVienXuatSac = dsnv.filter(function (nv) {
+    return nv.chucVu == 1 && nv.gioLam >= 192;
+  });
+
+  return nhanVienXuatSac.map(function (nv) {
+    return nv.name;
+  });
+}
+document.getElementById("nhanVienXS").onclick = function () {
+  var ketQuaXS = timNhanVienXuatSac() + " . ";
+  document.getElementById("ketQuaXS").innerHTML = ketQuaXS;
+};
+
+
+  // ! TÌM NV GIỎ
+  function timNhanVienGioi() {
+    var nhanVienGioi = dsnv.filter(function (nv) {
+      return nv.chucVu == 1 && nv.gioLam >= 176 && nv.gioLam < 192;
+    });
+  
+    return nhanVienGioi.map(function (nv) {
+      return nv.name;
+    });
+  }
+  document.getElementById("nhanVienG").onclick = function () {
+    var ketQuaGioi = timNhanVienGioi() + " . ";
+    document.getElementById("ketQuaGioi").innerHTML = ketQuaGioi;
+  };
+
+
+  // ! TÌM NV KHÁ
+  function timNhanVienKha() {
+    var nhanVienKha = dsnv.filter(function (nv) {
+      return nv.chucVu == 1 && nv.gioLam >= 160 && nv.gioLam < 176;
+    });
+  
+    return nhanVienKha.map(function (nv) {
+      return nv.name;
+    });
+  }
+
+  document.getElementById("nhanVienK").onclick = function () {
+    var ketQuaKha = timNhanVienKha() + " . ";
+    document.getElementById("ketQuaKha").innerHTML = ketQuaKha;
+  };
+  
+ 
+// ! TÌM NV TB 
+  function timNhanVienTrungBinh() {
+    var nhanVienTrungBinh = dsnv.filter(function (nv) {
+      return nv.chucVu == 1 && nv.gioLam < 160;
+    });
+  
+    return nhanVienTrungBinh.map(function (nv) {
+      return nv.name;
+    });
+  }
+  document.getElementById("nhanVienTB").onclick = function () {
+    var ketQuaTB = timNhanVienTrungBinh() + " . ";
+    document.getElementById("ketQuaTB").innerHTML = ketQuaTB;
+  };
+  
+
+  renderNv();
+  
